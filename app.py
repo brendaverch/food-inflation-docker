35eaa469b8a1ba1d3ee9981d4b1fa0eb2bc9954b
Brendaverch <brenda_verch@hotmail.com>
2025-04-26 14:50:47 -0300

    Corrige erros flake8 e configura virtualenv

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+
 
 def main(data_path="CPI_dataset.csv"):
 
